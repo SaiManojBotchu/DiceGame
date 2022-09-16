94702fcd97d43c401d1696f60fe5aa65ece5d097
Sai Manoj <saimanoj2826@gmail.com>
2022-09-16 19:19:41 +0530

    add roll dice logic

--- src/RollDice.js
+++ src/RollDice.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 import Die from './Die';
 
+const diceFaces = ["", "one", "two", "three", "four", "five", "six"];
 class RollDice extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dice1: 1,
+      dice2: 1
+    }
+    this.roll = this.roll.bind(this);
+  }
+  roll() {
+    let rand1 = Math.floor(Math.random() * 6) + 1;
+    let rand2 = Math.floor(Math.random() * 6) + 1;
+    this.setState({ dice1: rand1 , dice2: rand2 });
+  }
   render() {
     return (
       <div>
-        <h1>RollDice</h1>
-        <Die face="one" />
-        <Die face="two" />
+        <Die face={diceFaces[this.state.dice1]} />
+        <Die face={diceFaces[this.state.dice2]} />
+        <button onClick={this.roll}>Roll Dice!</button>
       </div>
     );
   }
